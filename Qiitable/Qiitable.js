300c8f8bf7383f55d7e56d9254322c3f8cf5304d
Kynako <Kynako@users.noreply.github.com>
2021-04-20 22:25:39 +0900

    [ADD] Qiitable.js

---
+++ Qiitable/Qiitable.js
@@ -0,0 +1,56 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-green; icon-glyph: book-open;
+/*!
+ * Qiitable.js v0.0.0
+ *
+ * Copyright (c) 2021 Kynako
+ *
+ * This software is released under the MIT license.
+ * See https://github.com/Kynako/Scriptable/blob/main/Qiitable/LICENSE
+*/
+
+class Qiitable {
+  constructor(access_token){
+    this.AT = access_token || null;
+    this.base_url = "https://qiita.com";
+  };
+    
+  async loadJSON(method, endpoint, param){
+    if(method.match(/^get$/i)){
+      const url = param == undefined
+        ? this.base_url + endpoint
+        : this.base_url + endpoint + this._buildQueryString(param);
+      const r = new Request(url);
+      r.method = method.toUpperCase();
+      r.headers = {"Authorization": "Bearer " + this.AT};
+      return {
+        json: await r.loadJSON(),
+        response: r.response
+      };
+    } else if(method.match(/^(post|patch|delete)$/i)){
+      const url = this.base_url + endpoint;
+      const r = new Request(url);
+      r.method = method.toUpperCase();
+      r.headers = {"Authorization": "Bearer " + this.AT};
+      r.body = param;
+      return {
+        json: await r.loadJSON(),
+        response: r.response
+      };
+    } else {
+      // The {method} isn't supported
+      console.error("ERROR");
+    };
+  };
+  
+  _buildQueryString(param){
+    if(param != undefined){
+      return "?" + Object.keys(param).sort().map((k)=>{
+        return `${k}=${encodeURIComponent(param[k])}`;
+      }).join("&")
+    } else return ""
+  };
+};
+
+module.exports = Qiitable;
